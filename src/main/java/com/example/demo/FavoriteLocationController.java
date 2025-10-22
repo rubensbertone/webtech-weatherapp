a942f1d7150e08d696aa7e59a344e974e53f1111
rubensbertone <Rubens.Bertone@student.htw-berlin.de>
2025-10-22 11:15:18 +0200

    CrossOrigin localhost erlaubt für Frontend

--- src/main/java/com/example/demo/FavoriteLocationController.java
+++ src/main/java/com/example/demo/FavoriteLocationController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class FavoriteLocationController {
 
     @GetMapping("/favoriteLocations")
